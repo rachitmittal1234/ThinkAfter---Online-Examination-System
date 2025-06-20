4bd018434e887c9793b35d3c4d89f5194939e238
rachitmittal1234 <rachitmittal696@gmail.com>
2025-06-21 00:42:43 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,1 +1,13 @@
+import app from '../server.js';
+import { createServer } from 'http';
+import { parse } from 'url';
+
+// Export a handler that Vercel can invoke
+export default async function handler(req, res) {
+  const server = createServer(app);
+  const parsedUrl = parse(req.url, true);
+
+  // Forward the request to the Express app
+  server.emit('request', req, res);
+}
 
